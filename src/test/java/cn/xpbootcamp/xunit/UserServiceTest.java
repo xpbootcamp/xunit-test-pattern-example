3dd758ac09c6063fd33f121b77fb3e29c477e214
QingQing Dou <qqdou@thoughtworks.com>
2020-09-16 00:44:16 +0800

    add login false test
    Signed-off-by: QingQing Dou <qqdou@thoughtworks.com>

--- src/test/java/cn/xpbootcamp/xunit/UserServiceTest.java
+++ src/test/java/cn/xpbootcamp/xunit/UserServiceTest.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class UserServiceTest {
+
     @Test
     public void should_login_success_when_user_login_given_valid_user_name_and_password() {
         String userName = "lisa";
@@ -19,6 +20,21 @@ public class UserServiceTest {
         String token = service.login(userName, password);
 
         Assertions.assertNotNull(token);
+
+        //GC(garbage-collect)销毁数据
+    }
+
+    @Test
+    public void should_login_fail_when_user_login_given_invalid_user_name_and_password() {
+        String invalidUserName = "li,_sa";
+        String password = "lisa123";
+        UserRepo userRepoTestStub = mock(UserRepo.class);
+        when(userRepoTestStub.getUserBy(invalidUserName, password)).thenReturn(false);
+        UserService service = new UserService(userRepoTestStub);
+
+        String token = service.login(invalidUserName, password);
+
+        Assertions.assertNull(token);
 
         //GC(garbage-collect)销毁数据
     }
