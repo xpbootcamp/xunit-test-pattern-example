b08f93ce9891cce37d916964d8c8d206ee90be86
QingQing Dou <qqdou@thoughtworks.com>
2020-09-23 13:05:57 +0800

    refactor code
    Signed-off-by: QingQing Dou <qqdou@thoughtworks.com>

--- src/test/java/cn/xpbootcamp/xunit/UserServiceTest.java
+++ src/test/java/cn/xpbootcamp/xunit/UserServiceTest.java
@@ -20,8 +20,7 @@ public class UserServiceTest {
         String userName = "lisa";
         String password = "lisa123";
         UserRepoTestStub userRepoTestStub = new UserRepoTestStub();
-        AuditLogServiceTestSpy logServiceTestSpy = new AuditLogServiceTestSpy();
-        UserService service = new UserService(userRepoTestStub, logServiceTestSpy);
+        UserService service = new UserService(userRepoTestStub, new AuditLogService());
 
         String token = service.login(userName, password);
 
@@ -36,8 +35,7 @@ public class UserServiceTest {
         String password = "lisa123";
         UserRepo userRepoTestStub = mock(UserRepo.class);
         when(userRepoTestStub.getUserBy(userName)).thenReturn(new User(userName, password));
-        AuditLogServiceTestSpy logServiceTestSpy = new AuditLogServiceTestSpy();
-        UserService service = new UserService(userRepoTestStub, logServiceTestSpy);
+        UserService service = new UserService(userRepoTestStub, new AuditLogService());
 
         String token = service.login(userName, password);
 
@@ -51,9 +49,8 @@ public class UserServiceTest {
         //手动实现Spy
         String userName = "lisa";
         String password = "lisa123";
-        UserRepoTestStub userRepoTestStub = new UserRepoTestStub();
         AuditLogServiceTestSpy logServiceTestSpy = new AuditLogServiceTestSpy();
-        UserService service = new UserService(userRepoTestStub, logServiceTestSpy);
+        UserService service = new UserService(new UserRepoTestStub(), logServiceTestSpy);
 
         String token = service.login(userName, password);
 
@@ -69,9 +66,8 @@ public class UserServiceTest {
     public void should_audit_log_when_user_login_success_given_valid_user_name_and_password_use_framework_spy() {
         String userName = "lisa";
         String password = "lisa123";
-        UserRepoTestStub userRepoTestStub = new UserRepoTestStub();
         AuditLogService auditLogServiceTestSpy = spy(AuditLogService.class);
-        UserService service = new UserService(userRepoTestStub, auditLogServiceTestSpy);
+        UserService service = new UserService(new UserRepoTestStub(), auditLogServiceTestSpy);
 
         String token = service.login(userName, password);
 
@@ -89,8 +85,7 @@ public class UserServiceTest {
         String password = "lisa123";
         UserRepo userRepoMockObject = mock(UserRepo.class);
         when(userRepoMockObject.getUserBy(userName)).thenReturn(null);
-        AuditLogServiceTestSpy logServiceTestSpy = new AuditLogServiceTestSpy();
-        UserService service = new UserService(userRepoMockObject, logServiceTestSpy);
+        UserService service = new UserService(userRepoMockObject, new AuditLogService());
 
         service.register(userName, password);
 
@@ -106,8 +101,7 @@ public class UserServiceTest {
         String password = "lisa123";
         UserRepoFakeObject repoFakeObject = new UserRepoFakeObject();
         repoFakeObject.save(new User(userName, password));
-        AuditLogServiceTestSpy logServiceTestSpy = new AuditLogServiceTestSpy();
-        UserService service = new UserService(repoFakeObject, logServiceTestSpy);
+        UserService service = new UserService(repoFakeObject, new AuditLogService());
 
         String token = service.login(userName, password);
 
@@ -120,31 +114,12 @@ public class UserServiceTest {
     public void should_login_success_when_user_login_given_valid_user_name_and_password_use_dummy() {
         String userName = "lisa";
         String password = "lisa123";
-        UserRepo userRepo = new UserRepo();
-        userRepo.save(new User(userName, password));
-        AuditLogServiceTestSpy logServiceTestSpy = new AuditLogServiceTestSpy();
-        UserService service = new UserService(userRepo, logServiceTestSpy);
+        UserService service = new UserService(new UserRepoTestStub(), new AuditLogService());
 
         boolean dummyObject = true;
         String token = service.login(userName, password, dummyObject);
 
         Assertions.assertNotNull(token);
-
-        //GC(garbage-collect)销毁数据
-    }
-
-    @Test
-    public void should_login_fail_when_user_login_given_invalid_user_name_and_password() {
-        String invalidUserName = "li,_sa";
-        String password = "lisa123";
-        UserRepo userRepoTestStub = mock(UserRepo.class);
-        when(userRepoTestStub.getUserBy(invalidUserName)).thenReturn(new User("", ""));
-        AuditLogServiceTestSpy logServiceTestSpy = new AuditLogServiceTestSpy();
-        UserService service = new UserService(userRepoTestStub, logServiceTestSpy);
-
-        String token = service.login(invalidUserName, password);
-
-        Assertions.assertNull(token);
 
         //GC(garbage-collect)销毁数据
     }
